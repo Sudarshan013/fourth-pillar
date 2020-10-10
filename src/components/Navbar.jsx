4ffedb0ce5b032ba23e21df771a9a4fab1097c63
Sudarshan <iyengarsudharshan6@gmail.com>
2020-10-11 04:06:45 +0530

    Landing page completion and added view all crime route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import {Alignment, Button, Navbar as Nav, NavbarDivider } from '@blueprintjs/core';
 import React, { Fragment, memo } from 'react'
+import { NavLink } from 'react-router-dom';
 
 export default memo(function Navbar(props) {
   return (
     <Fragment>
       <Nav>
         <Nav.Group>
-          <Nav.Heading className="nav__link">Fourth Pillar</Nav.Heading>
+          <NavLink to="/" activeClassName="home__link">
+            <Nav.Heading className="nav__link">Fourth Pillar</Nav.Heading>
+          </NavLink>
           {props.children}
         </Nav.Group>
       </Nav>
