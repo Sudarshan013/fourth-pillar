b19df4165327a4ec557744d17b22fab6047d9d2c
sriram199916 <sriram199916@gmail.com>
2020-10-10 14:55:24 +0530

    Added basic functions

--- src/contracts/main.sol
+++ src/contracts/main.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.5.0;
-
+pragma experimental ABIEncoderV2;
 contract main {
 
   string[] topTen;
@@ -32,5 +32,26 @@ contract main {
   mapping (string => news[]) public newsForAcnt;
   mapping (string => string[]) public verifiedNews;
   mapping (uint => string) public identity;
+
+  function string_check(string memory str1, string memory str2) pure internal returns (bool) public{
+        return (keccak256(abi.encodePacked(str1)) == keccak256(abi.encodePacked(str2)));
+    }
+
+    function createAccount(string memory _id,string memory _name,uint _aadhaar,string memory _profile,bool _flag) public{
+      require(string_check(accounts[_id].id,''),'Id already in use!');
+      require(string_check(identity[_aadhaar],''),'Aadhaar id already in use!');
+      accounts[_id]=account(_flag,msg.sender,_id,_name,_profile,_aadhaar,0,0);
+      identity[_aadhaar]=_id;
+    }
+
+    function createNews(string memory _authid,uint _time,string memory _descreption,string memory _id,string memory _categories,string memory _subcategories,string memory _tagged) public{
+        newsById[_id]=news(_time,_descreption,_id,_categories,_subcategories,_tagged);
+        authorNews[_authid].push(newsById[_id]);
+    }
+
+    function setCategory(string memory _aid,string memory _nid,string memory _category) public{
+        require(string_check(accounts[_aid].id,_aid),'You are not author!');
+        newsByCategory[_category].push(_nid);
+    }
 
 }
